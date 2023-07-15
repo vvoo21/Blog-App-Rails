4ecd3a2d545f5f42eb62ded022fbb9bb80d075ba
vvoo21 <vaneolipa@gmail.com>
2023-07-15 11:14:50 -0500

    Fix the relationship in the Like model

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,7 +1,6 @@
 class Like < ApplicationRecord
-  has_many :posts, dependent: :destroy, foreign_key: :author_id
-  has_many :comments, dependent: :destroy
-  has_many :likes, dependent: :destroy
+  belongs_to :author, class_name: 'User', foreign_key: :author_id
+  belongs_to :post
   after_save :update_likes_count
   after_destroy :decrease_likes_count
 
