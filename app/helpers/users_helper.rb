4175447f7a6170b5cc4c8e94e2fa0d6efeeb8b50
vvoo21 <vaneolipa@gmail.com>
2023-07-21 23:44:51 -0500

    Delete display_user_photo method

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,9 +1,2 @@
 module UsersHelper
-  def display_user_photo(user)
-    if user.photo.present?
-      image_tag(user.photo, alt: 'User photo', width: 200, height: 150)
-    else
-      image_tag('photo.jpg', alt: 'User photo', width: 200, height: 150)
-    end
-  end
 end
